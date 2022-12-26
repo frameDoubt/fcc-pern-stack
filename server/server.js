92013954d21f69dd3a3572904a1129cad23e6b49
Brandon Wright <brandon.w.wright4@gmail.com>
2022-12-26 07:37:29 -0800

    add dotenv variable and port constant

--- server/server.js
+++ server/server.js
@@ -1,7 +1,10 @@
+require('dotenv').config();
+
 const express = require("express");
 
 const app = express();
 
-app.listen(3005, () => {
-    console.log("server is up and listening on port 3005");
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+    console.log(`server is up and listening on port ${PORT}`);
 });
