9d9dec98d0d6556d52a99cf03f2797d4239f12d5
Brandon Wright <brandon.w.wright4@gmail.com>
2022-12-27 07:30:00 -0800

    add pg configuration

--- server/server.js
+++ server/server.js
@@ -2,11 +2,14 @@ require('dotenv').config();
 const express = require("express");
 const morgan = require("morgan");
 const app = express();
+const db = require("./db");
 
 app.use(express.json());
 
 // get all restaurants
-app.get("/api/v1/restaurants", (req, res) => {
+app.get("/api/v1/restaurants", async (req, res) => {
+    const results = await db.query("SELECT * FROM restaurants");
+    console.log(results);
     res.status(200).json({
         status: "success",
         data: {
